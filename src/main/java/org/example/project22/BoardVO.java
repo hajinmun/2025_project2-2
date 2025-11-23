735991166f1e33b1ca80554d4ca3794dd7ab3f62
hajinmun <mmdaggu2@gmail.com>
2025-11-23 21:31:02 +0900

    feat: BoardDAO구현

--- src/main/java/org/example/project22/BoardVO.java
+++ src/main/java/org/example/project22/BoardVO.java
@@ -11,6 +11,13 @@ public class BoardVO {
     private Date regdate;
     private int cnt;
 
+
+    public BoardVO(String title, String writer, String content) {
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
     public int getId() {
         return id;
     }
